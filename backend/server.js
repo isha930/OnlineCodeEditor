f5483b171d34bf9893142b1645d6b7bbb292e232
isha930 <146696659+isha930@users.noreply.github.com>
2025-03-15 20:40:24 +0530

    updated port

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
 app.use(cors());
